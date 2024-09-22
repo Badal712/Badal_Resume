e3895085a59d7102d8e061bfc0036e2ff27c4429
Badal Sahoo <135866766+Badal712@users.noreply.github.com>
2024-09-22 20:27:40 +0530

    Adding programming skills to the circular progress bar

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ window.onload = function () {
         }, 15);
     });
 };
+
+
 // Show popup for "Lecturer" when "Read more" is clicked
 document.querySelector('.view-more').addEventListener('click', function() {
   document.getElementById('popup').style.display = 'flex';  // Show the Lecturer popup window
@@ -33,3 +35,74 @@ document.querySelector('.read-more').addEventListener('click', function() {
 document.querySelector('.close-popup1').addEventListener('click', function() {
   document.getElementById('popup1').style.display = 'none';  // Hide the Lab Assistant popup window
 });
+
+
+
+//Circular Progress Bar for HTML % increased
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+  if (counter == 80){
+    clearInterval();
+  }
+  else{
+    counter += 1;
+    number.innerHTML = counter + "%";
+  }
+}, 26);
+
+
+//Circular Progress Bar for CSS % increased
+let number2 = document.getElementById("number2");
+let counter1 = 0;
+setInterval(() => {
+  if (counter1 == 75){
+    clearInterval();
+  }
+  else{
+    counter1 += 1;
+    number2.innerHTML = counter1 + "%";
+  }
+}, 27);
+
+
+//Circular Progress Bar for JavaScript % increased
+let number3 = document.getElementById("number3");
+let counter2 = 0;
+setInterval(() => {
+  if (counter2 == 60){
+    clearInterval();
+  }
+  else{
+    counter2 += 1;
+    number3.innerHTML = counter2 + "%";
+  }
+}, 33);
+
+
+//Circular Progress Bar for MongoDB % increased
+let number4 = document.getElementById("number4");
+let counter3 = 0;
+setInterval(() => {
+  if (counter3 == 60){
+    clearInterval();
+  }
+  else{
+    counter3 += 1;
+    number4.innerHTML = counter3 + "%";
+  }
+}, 33);
+
+
+//Circular Progress Bar for MongoDB % increased
+let number5 = document.getElementById("number5");
+let counter4 = 0;
+setInterval(() => {
+  if (counter4 == 50){
+    clearInterval();
+  }
+  else{
+    counter4 += 1;
+    number5.innerHTML = counter4 + "%";
+  }
+}, 40);
